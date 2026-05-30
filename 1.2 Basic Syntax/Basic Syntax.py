b7b143e70facdd2dcf1d646fa0e38a3022d259dd
MasArik09 <munadilajbg5@gmail.com>
2026-05-30 11:16:26 +0700

    Initial commit - isi modul 1

---
+++ 1.2 Basic Syntax/Basic Syntax.py
@@ -0,0 +1,9 @@
+"""
+apalah conah katanya sih komentar banyak baris
+"""
+print("Hello!")
+print("angka:", 42)
+print("Dua nilai:", "Nama", "Andi")
+
+nama = input("Siapa namamu?")
+print("Halo", nama)
