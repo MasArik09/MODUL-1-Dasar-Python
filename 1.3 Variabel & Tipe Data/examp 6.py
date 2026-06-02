f839c511b5629da2af8a19e9d824dd6f99003b7f
MasArik09 <munadilajbg5@gmail.com>
2026-06-02 21:02:52 +0700

    Menambahkan example 6

---
+++ 1.3 Variabel & Tipe Data/examp 6.py
@@ -0,0 +1,6 @@
+# 1. .strip() -> Menghilangkan spasi tidak berguna di awal dan akhir string
+email_user = "   user@email.com   "
+print("Sebelum strip:", email_user)
+print("Setelah strip:", email_user.strip()) # Output: "user@email.com"
+
+# 2. .split() -> Memecah string menjadi List berdasarkan separator tertentu
